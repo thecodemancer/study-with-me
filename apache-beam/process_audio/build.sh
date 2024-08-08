350a5d8c0a3513b3adc4d3ff134d7d828f85e84c
David Regalado <davidregalado12@hotmail.com>
2024-08-08 22:05:44 +0000

    process_audio: it worked!

--- apache-beam/process_audio/build.sh
+++ apache-beam/process_audio/build.sh
@@ -1,7 +1,6 @@
 gcloud dataflow flex-template build gs://$BUCKET/df_test.json \
- --image "${REGION}-docker.pkg.dev/${PROJECT}/process-audio/df-template:latest" \
- --project "${PROJECT}" \
- --network "${NETWORK}" \
- --subnetwork "${SUBNETWORK}" \
- --sdk-language "PYTHON" \
- --metadata-file "metadata.json"
+ --image="${REGION}-docker.pkg.dev/${PROJECT}/process-audio/df-template:latest" \
+ --network="${NETWORK}" \
+ --subnetwork="${SUBNETWORK}" \
+ --sdk-language="PYTHON" \
+ --metadata-file="metadata.json"
